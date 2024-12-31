c0e5e5275c505d2886797f69e0fb4b249344e20d
natan-marinho <natan.marinho.profissional@gmail.com>
2024-12-31 00:15:14 -0300

    AddPhotos

--- src/app/(pages)/home/projects.ts
+++ src/app/(pages)/home/projects.ts
@@ -3,9 +3,9 @@ export const projects = [
     title: "Projeto 1",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/projectOne/capa1.jpeg",
-      "/projectOne/img2.jpg",
-      "/projectOne/img3.png"
+      "/proj1/1.jpg",
+      "/proj1/2.jpg",
+      "/proj1/3.jpg"
     ],
     currentImage: 0,
     tags: ["React", "Node.js", "MongoDB", "MySQL"],
@@ -15,60 +15,47 @@ export const projects = [
     title: "Projeto 2",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/projectOne/capa1.jpeg",
-      "/projectOne/img2.jpg",
-      "/projectOne/img3.png"
+      "/proj2/1.jpg",
+      "/proj2/2.jpg",
+      "/proj2/3.jpg"
     ],
     currentImage: 0,
-    tags: ["TypeScript", "Next.js", "PostgreSQL"],
-    githubUrl: "https://github.com/seu-usuario/projeto-2"
-  },
-  {
-    title: "Projeto 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
-    images: [
-      "/projectOne/capa1.jpeg",
-      "/projectOne/img2.jpg",
-      "/projectOne/img3.png"
-    ],
-    currentImage: 0,
-    tags: ["TypeScript", "Next.js", "PostgreSQL"],
-    githubUrl: "https://github.com/seu-usuario/projeto-2"
-  },
-  {
-    title: "Projeto 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
-    images: [
-      "/projectOne/capa1.jpeg",
-      "/projectOne/img2.jpg",
-      "/projectOne/img3.png"
-    ],
-    currentImage: 0,
-    tags: ["TypeScript", "Next.js", "PostgreSQL"],
-    githubUrl: "https://github.com/seu-usuario/projeto-2"
-  },
-  {
-    title: "Projeto 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
-    images: [
-      "/projectOne/capa1.jpeg",
-      "/projectOne/img2.jpg",
-      "/projectOne/img3.png"
-    ],
-    currentImage: 0,
-    tags: ["TypeScript", "Next.js", "PostgreSQL"],
-    githubUrl: "https://github.com/seu-usuario/projeto-2"
+    tags: ["React", "Node.js", "MongoDB", "MySQL"],
+    githubUrl: "https://github.com/seu-usuario/projeto-1"
   },
   {
     title: "Projeto 3",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/projectOne/capa1.jpeg",
-      "/projectOne/img2.jpg",
-      "/projectOne/img3.png"
+      "/proj3/1.jpg",
+      "/proj3/2.jpg",
+      "/proj3/3.jpg"
     ],
     currentImage: 0,
-    tags: ["React Native", "Firebase"],
-    githubUrl: "https://github.com/seu-usuario/projeto-3"
+    tags: ["React", "Node.js", "MongoDB", "MySQL"],
+    githubUrl: "https://github.com/seu-usuario/projeto-1"
+  },
+  {
+    title: "Projeto 4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
+    images: [
+      "/proj4/1.jpg",
+      "/proj4/2.jpg"
+    ],
+    currentImage: 0,
+    tags: ["React", "Node.js", "MongoDB", "MySQL"],
+    githubUrl: "https://github.com/seu-usuario/projeto-1"
+  },
+  {
+    title: "Projeto 5",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
+    images: [
+      "/proj5/1.jpg",
+      "/proj5/2.jpg",
+      "/proj5/3.jpg"
+    ],
+    currentImage: 0,
+    tags: ["React", "Node.js", "MongoDB", "MySQL"],
+    githubUrl: "https://github.com/seu-usuario/projeto-1"
   }
 ];
