14f486668393fab130529c265f0b8518c8972cf4
natan-marinho <natan.marinho.profissional@gmail.com>
2025-01-03 17:29:38 -0300

    UpdateImagesResolution

--- src/app/(pages)/home/projects.ts
+++ src/app/(pages)/home/projects.ts
@@ -3,9 +3,9 @@ export const projects = [
     title: "Projeto 1",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/proj1/1.jpg",
-      "/proj1/2.jpg",
-      "/proj1/3.jpg"
+      "/proj1/1.png",
+      "/proj1/2.png",
+      "/proj1/3.png"
     ],
     currentImage: 0,
     tags: ["React", "Node.js", "MongoDB", "MySQL"],
@@ -15,9 +15,9 @@ export const projects = [
     title: "Projeto 2",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/proj2/1.jpg",
-      "/proj2/2.jpg",
-      "/proj2/3.jpg"
+      "/proj2/1.png",
+      "/proj2/2.png",
+      "/proj2/3.png"
     ],
     currentImage: 0,
     tags: ["React", "Node.js", "MongoDB", "MySQL"],
@@ -27,9 +27,9 @@ export const projects = [
     title: "Projeto 3",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/proj3/1.jpg",
-      "/proj3/2.jpg",
-      "/proj3/3.jpg"
+      "/proj3/1.png",
+      "/proj3/2.png",
+      "/proj3/3.png"
     ],
     currentImage: 0,
     tags: ["React", "Node.js", "MongoDB", "MySQL"],
@@ -39,8 +39,8 @@ export const projects = [
     title: "Projeto 4",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/proj4/1.jpg",
-      "/proj4/2.jpg"
+      "/proj4/1.png",
+      "/proj4/2.png"
     ],
     currentImage: 0,
     tags: ["React", "Node.js", "MongoDB", "MySQL"],
@@ -50,9 +50,9 @@ export const projects = [
     title: "Projeto 5",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur odio vel dui molestie efficitur. Nunc ut tellus lacus. Donec sit amet neque nibh. Nullam vel condimentum odio. Aenean sed pretium tellus. Praesent rhoncus elementum nisi non posuere. Curabitur vel ullamcorper nisl. Nam pretium lacus nisl, quis tincidunt sem eleifend non.",
     images: [
-      "/proj5/1.jpg",
-      "/proj5/2.jpg",
-      "/proj5/3.jpg"
+      "/proj5/1.png",
+      "/proj5/2.png",
+      "/proj5/3.png"
     ],
     currentImage: 0,
     tags: ["React", "Node.js", "MongoDB", "MySQL"],
